0982fc5614401c68d6698af494ae5fc5610a4c38
Superior125 <90008321+Superior126@users.noreply.github.com>
2023-06-14 16:01:53 -0400

    setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./pages/main";
+import Login from "./pages/login"
+import NewPost from "./pages/post";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/new_post" element={<NewPost />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/post.js
@@ -0,0 +1,9 @@
+function NewPost() {
+    return(
+        <div>
+            <h1>Post Page</h1>
+        </div>
+    )
+}
+
+export default NewPost;
