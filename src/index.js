a0dd113750ae1568171db3bb15f2243f77a88e9e
Superior125 <90008321+Superior126@users.noreply.github.com>
2024-05-01 12:02:48 -0400

    Add Sentry Support

--- src/index.js
+++ src/index.js
@@ -2,7 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as Sentry from "@sentry/react";
 import reportWebVitals from './reportWebVitals';
+
+// Enable Sentry in the production environment only
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    dsn: "https://d30ff5af6215c8725419c076e1c3913b@o4507181227769856.ingest.us.sentry.io/4507181232226304",
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: ["localhost", /^https:\/\/api\.support\.lifplatforms\.com\//],
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
+} else {
+  console.log("App is not running in production mode. Sentry is DISABLED!");
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
