1105903a8e91a82f7b99b355fd17714f259b4229
Superior125 <90008321+Superior126@users.noreply.github.com>
2024-06-09 14:48:14 -0400

    Fix code scanning issues

--- src/scripts/sanitize_username.js
+++ src/scripts/sanitize_username.js
@@ -1,11 +1,17 @@
+/**
+ * Escapes special HTML characters in a given string.
+ *
+ * @param {string} text - The input string to escape.
+ * @returns {string} - The escaped string.
+ */
 export default function escapeHtml(text) {
     const map = {
         "&": "&amp;",
         "<": "&lt;",
         ">": "&gt;",
         '"': "&quot;",
-        "'": "&#039;",
+        "'": "&#39;",
     };
 
-    return text.replace(/[&<>"']/g, (char) => map[char]);
-};
+    return text.replace(/[&<>"']/g, (char) => map.char = char);
+}
