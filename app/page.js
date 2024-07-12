33edb4eeb1687f96892a58a85583b3e47d01c20f
Superior125 <90008321+Superior126@users.noreply.github.com>
2024-07-12 12:33:07 -0400

    Updates

--- app/page.js
+++ app/page.js
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+export const metadata = {
+  title: "Lif Support",
+  description: "Get support from our helpful community for Lif Platforms products and services."
+}
+
 export default function Home() {
   return (
     <main>
