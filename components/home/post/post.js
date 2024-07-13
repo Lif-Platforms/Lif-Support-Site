377cd76c8306db35ef8138a46f50d20056ea10eb
Superior125 <90008321+Superior126@users.noreply.github.com>
2024-07-12 21:05:39 -0400

    Add main page

---
+++ components/home/post/post.js
@@ -0,0 +1,20 @@
+'use client'
+
+import styles from './post.module.css';
+import { useRouter } from 'next/navigation';
+
+export default function Post({title, content, software, id}) {
+    const router = useRouter();
+
+    function handle_navigation() {
+        router.push(`/post/${id}/${title}`);
+    }
+
+    return (
+        <div className={styles.post} onClick={handle_navigation}>
+            <h1>{title}</h1>
+            <p>{content}</p>
+            <span className={software === "Ringer" ? styles.ringer_software : software === "Dayly" ? styles.dayly_software : styles.software}>{software}</span>
+        </div>
+    )
+}
